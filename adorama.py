f706866df96fb96403a200a116be21d1d40c26fd
crysmah <x.jdiaz194@gmail.com>
2023-08-01 18:30:02 -0700

    Added function scrape_adorama

--- adorama.py
+++ adorama.py
@@ -5,33 +5,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome()
+def scrape_adorama(search_query, output_file):
+    driver = webdriver.Chrome()
 
-driver.get("https://www.adorama.com/")
+    try:
+        driver.get("https://www.adorama.com/")
+        
+        # Find the search input field using the appropriate locating strategy
+        search_input = driver.find_element(By.NAME, "searchinfo")
+        search_input.send_keys(search_query)
+        search_input.send_keys(Keys.RETURN)
 
-# Find the search input field using the appropriate locating strategy
-search = driver.find_element(By.NAME, "searchinfo")
+        names = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "Products_title__t1ag6"))
+        )
+        prices = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "Products_price__YsZAR"))
+        )
 
-search.send_keys("radeon rx graphics card")
-search.send_keys(Keys.RETURN)
+        with open(output_file, "w") as file:
+            for name, price in zip(names, prices):
+                output_line = f"{name.text} {price.text}\n"
+                print(output_line)
+                file.write(output_line)
 
-try:
-    names = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "Products_title__t1ag6"))
-    )
-    prices = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "Products_price__YsZAR"))
-    )
+    except Exception as e:
+        print("An error occurred:", e)
 
-    with open("output.txt", "w") as file:
-        for name, price in zip(names, prices):
-            output_line = f"{name.text} {price.text}\n"
-            print(output_line)
-            file.write(output_line)
+    finally:
+        time.sleep(5)
+        driver.quit()
 
-except Exception as e:
-    print("An error occurred:", e)
-
-finally:
-    time.sleep(5)
-    driver.quit()
+if __name__ == "__main__":
+    search_query = "radeon rx graphics card"
+    output_file = "output.txt"
+    scrape_adorama(search_query, output_file)
